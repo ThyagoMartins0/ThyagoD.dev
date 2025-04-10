45d3cb94f21eff856cf2e785dba88ac6e3562e82
Thyago Martins <thyago.martins@esamcsantos.onmicrosoft.com>
2025-04-10 01:12:05 -0300

    multivideo
    implementando sistema de videos baseado em idioma

--- src/app/components/video/Video.tsx
+++ src/app/components/video/Video.tsx
@@ -1,6 +1,22 @@
+'use client';
+
+import { useLanguage } from '../../i18n/LanguageContext';
 import styles from './Video.module.css'
 
 export default function Video() {
+  const { language } = useLanguage();
+
+  const getVideoUrl = () => {
+    switch (language) {
+      case 'en':
+        return 'https://www.youtube.com/embed/ox2cInj1wn4';
+      case 'es':
+        return 'https://www.youtube.com/embed/MuNyfqxAO6Y';
+      default:
+        return 'https://www.youtube.com/embed/NNU6easVvRI';
+    }
+  };
+
   return (
     <section className={styles.video}>
       <div className={styles.videoContent}>
@@ -11,7 +27,7 @@ export default function Video() {
         <div className={styles.videoWrapper}>
           <iframe
             className={styles.videoIframe}
-            src="https://www.youtube.com/embed/NNU6easVvRI"
+            src={getVideoUrl()}
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
